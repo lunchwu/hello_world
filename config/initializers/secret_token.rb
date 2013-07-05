cb495d779a1dde7bdecbacd9fc8249c12b0befb1
fwu <lunchwu@yeah.net>
2013-07-05 23:09:07 +0800

    hello world ruby projects
    RT
    
    Signed-off-by: fwu <lunchwu@yeah.net>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'cc83d4d5bfa017c11dbe5e7295323765692f27c34eb672209a7c323a7feeee88da21bbe774aebdbf079a6b19c70f37c3cdd75b840add6e7af1c399b30c74454a'
